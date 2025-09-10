5ec3ee38e68fabc217fb480c1d0b67b378c2f43b
mial-92 <mialin0170@gmail.com>
2025-09-10 07:30:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,25 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-t.forward(200)
 turtle.done
-def message(input):
-    print(input)
-message("Hello Class")
+def message(x):
+    print('hello')
+
+def square(x,y):
+    t.forward(x)
+    t.left(90)
+    t.forward(y)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(y)
+    t.left(90)
+square(125,100)
+t.forward(120)
+def equal(a):
+    t.forward(a)
+    t.left(120)
+    t.forward(a)
+    t.left(120)
+    t.forward(a)
+equal(90)

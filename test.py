d24a71a87511d6ea9caf6029cb3e86b77ad5c756
mial-92 <mialin0170@gmail.com>
2025-09-11 06:59:01 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,15 @@ turtle.done
 def message(x):
     print('hello')
 
+
+""" def equal(a):
+    t.forward(a)
+    t.left(120)
+    t.forward(a)
+    t.left(120)
+    t.forward(a)
+equal(90)  """
+""" 
 def square(x,y):
     t.forward(x)
     t.left(90)
@@ -16,11 +25,32 @@ def square(x,y):
     t.forward(y)
     t.left(90)
 square(125,100)
-t.forward(120)
-def equal(a):
-    t.forward(a)
-    t.left(120)
-    t.forward(a)
-    t.left(120)
-    t.forward(a)
-equal(90)
+
+size = 35 
+for i in range(60):
+    square(size, size)
+    t.left(5)
+    size += 5
+
+    t.speed(0)
+ """
+def spiralstar(x):
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144) 
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
+    t.forward(x)
+    t.left(144)
+spiralstar(144)
+
+size = 35
+for i in range(60):
+    spiralstar(size)
+    t.left(5)
+    size += 5
+    
+    t.speed(0)
